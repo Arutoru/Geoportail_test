e69e1e0c26c9f8824a3b4be80383ab1722141817
Arutoru <arthurtankwa@gmail.com>
2023-01-04 11:47:21 +0000

    First commit to the geoportail project test

---
+++ reporter/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.contrib.gis.db import models
+from django.contrib.gis.utils import LayerMapping
+from django.contrib.gis.geos import Point
+from django.db.models import Manager as GeoManager
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+Imagepath = os.path.join(BASE_DIR, 'defaut.jpg')
+
+# Create your models here.
+class Borne(models.Model):
+    name = models.CharField(max_length=250)
+    east = models.FloatField()
+    nord = models.FloatField()
+    picture = models.ImageField(default=Imagepath)#on utilise défault pour l'image par défaut
+    geom = models.PointField(srid=4326)
+    objects = GeoManager()
+
+    def __unicode__(self):
+        return self.name
+    @property
+    def picture_url(self):
+        return self.picture.url
+
+    class meta:
+        verbose_name_plural = 'Bornes'
+
+class Region(models.Model):
+    sce_geo = models.CharField(max_length=50)
+    sce_sem = models.CharField(max_length=50)
+    date = models.DateField()
+    origine = models.CharField(max_length=50)
+    region = models.CharField(max_length=3)
+    nom = models.CharField(max_length=50)
+    superficie = models.FloatField()
+    geom = models.MultiPolygonField(srid=4326)
+
+    def _unicode_(self):
+        return self.nom
---
+++ reporter/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import HomePageView
+from djgeojson.views import GeoJSONLayerView
+from reporter.models import Region, Borne
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns= [
+    path('', HomePageView.as_view(), name='home'),
+    path('accounts/profile/', HomePageView.as_view(), name='home'),
+    path('borne_data/', GeoJSONLayerView.as_view(model=Borne, properties=('name','picture_url','east','nord')), name='borne_data'),
+    path('region_data/', GeoJSONLayerView.as_view(model=Region, properties=('nom', 'date', 'superficie')), name='region_data')
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
