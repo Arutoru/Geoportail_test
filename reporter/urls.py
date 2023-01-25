8ed90fed6ffde389d31299d40026f92f162d1b18
Arutoru <arthurtankwa@gmail.com>
2023-01-25 10:19:45 +0000

    listView Beta version

--- reporter/urls.py
+++ reporter/urls.py
@@ -12,4 +12,4 @@ urlpatterns= [
     path('aot_data/', GeoJSONLayerView.as_view(model=Aot, properties=('amodiatair', 'niu', 'sup', 'date_caut')), name='aot_data')
     # path('borne_data/', GeoJSONLayerView.as_view(model=Borne, properties=('name','picture_url','east','nord')), name='borne_data'),
     # path('region_data/', GeoJSONLayerView.as_view(model=Region, properties=('nom', 'date', 'superficie')), name='region_data')
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
