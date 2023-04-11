ae32bce44940cb9e0b598acdecbf0a6d54731f1e
Arutoru <arthurtankwa@gmail.com>
2023-04-11 14:42:05 +0100

    Pre-deployment 7

--- agricom/settings.py
+++ agricom/settings.py
@@ -15,8 +15,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-GDAL_LIBRARY_PATH = os.path.join(BASE_DIR,'GDAL/gdal304')
-GEOS_LIBRARY_PATH = os.path.join(BASE_DIR,'GDAL/geos_c')
+GDAL_LIBRARY_PATH = os.path.join(BASE_DIR,'GDAL/gdal304.dll')
+GEOS_LIBRARY_PATH = os.path.join(BASE_DIR,'GDAL/geos_c.dll')
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 STATIC_DIR = os.path.join(BASE_DIR,'reporter/static')
 
