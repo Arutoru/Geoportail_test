4cd4ad6652996caa246c57106d0b9680c6fffa64
Arutoru <arthurtankwa@gmail.com>
2023-04-12 15:30:19 +0100

    test-deployment

--- agricom/settings.py
+++ agricom/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_DIR = BASE_DIR / 'reporter' / 'static'
-GDAL_LIBRARY_PATH = str(os.path.join(STATIC_DIR, "GDAL/gdal.nix"))
+GDAL_LIBRARY_PATH = str(os.path.join(STATIC_DIR, "GDAL/gdal"))
 GEOS_LIBRARY_PATH = str(os.path.join(STATIC_DIR, "GDAL/geos_c.dll"))
 
 # Quick-start development settings - unsuitable for production
