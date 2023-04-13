1b1dc6b3acc64870b92c4fdb12c24d011a2a4452
Arutoru <arthurtankwa@gmail.com>
2023-04-13 10:23:36 +0100

    Engine modification

--- agricom/settings.py
+++ agricom/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'agricom.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'postgresql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
         'HOST': 'containers-us-west-1.railway.app',
